222bd6edd05e324e1bf58469d8a2b9d74c94050b
aravindravi336 <aravindravi336@gmail.com>
2026-03-06 15:26:38 +0530

    config edit

--- tina/config.js
+++ tina/config.js
@@ -1,37 +1,22 @@
 import { defineConfig } from "tinacms";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
-
 export default defineConfig({
-  branch,
-
-  // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  branch: "main",
+  clientId: "380d1705-731f-4b71-9de5-27f802eb2e62",
+  token: "99eae15382ba19386efd06dacd12fcdd0d36280b",
 
   build: {
     outputFolder: "admin",
     publicFolder: ".",
   },
-  // Uncomment to allow cross-origin requests from non-localhost origins
-  // during local development (e.g. GitHub Codespaces, Gitpod, Docker).
-  // Use 'private' to allow all private-network IPs (WSL2, Docker, etc.)
-  // server: {
-  //   allowedOrigins: ['https://your-codespace.github.dev'],
-  // },
+
   media: {
     tina: {
-      mediaRoot: "",
+      mediaRoot: "assets/uploads",
       publicFolder: ".",
     },
   },
-  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
+
   schema: {
     collections: [
       {
