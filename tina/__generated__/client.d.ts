38376d06e3894f6860f5935a35fc8d3921612756
aravindravi336 <aravindravi336@gmail.com>
2026-03-06 14:44:11 +0530

    Update tina generated files

--- tina/__generated__/client.d.ts
+++ tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'E:/KaiMedia/Kai-Media/tina/__generated__/.cache/1772787724913', url: 'https://content.tinajs.io/2.2/content/380d1705-731f-4b71-9de5-27f802eb2e62/github/main', token: '99eae15382ba19386efd06dacd12fcdd0d36280b', queries,  });
+export const client = createClient({ cacheDir: 'E:/KaiMedia/Kai-Media/tina/__generated__/.cache/1772788257639', url: 'https://content.tinajs.io/2.2/content/380d1705-731f-4b71-9de5-27f802eb2e62/github/main', token: '99eae15382ba19386efd06dacd12fcdd0d36280b', queries,  });
 export default client;
   
